aae59b818e42569b7fc1c671c36cbb358ab11c2d
Léo Hubert <leo.hubert@bodyguard.ai>
2022-10-06 11:04:05 +0200

    v2

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const TEST = require("node-global-key-listener")
+const { app, BrowserWindow, Menu, MenuItem, globalShortcut } = require('electron')
 var player = require('play-sound')(opts = {})
-
-const v = new TEST.GlobalKeyboardListener();
 
 
 const sounds = [
@@ -9,10 +7,46 @@ const sounds = [
   'kraaa.m4a',
 ]
 
-v.addListener(function (e, down) {
-  if (e.state === "DOWN") {
-    player.play(sounds[Math.floor(Math.random()*sounds.length)], function(err){
-      if (err) throw err
+
+function playSound() {
+  player.play(sounds[Math.floor(Math.random()*sounds.length)], function(err){
+    if (err) throw err
+  })
+}
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 0,
+    height: 0,
+    show: false
+  })
+  
+  if (process.platform == 'darwin') {  
+    app.dock.hide()  
+  }
+}
+
+// Generate all keyboard keys values
+const keys = '`1234567890-=qwertyuiop[]asdfghjkl;\'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOPASDFGHJKL:"ZXCVBNM<>?'.split('')
+
+
+
+app.whenReady().then(() => {
+  createWindow()
+
+  globalShortcut.register('OPTION+Q', () => {
+    app.quit()
+  })
+
+
+  for (const key of keys) {
+    globalShortcut.register(key, () => {
+      playSound()
     })
   }
-});
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+
+})
