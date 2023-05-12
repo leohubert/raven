5083997fd21892ada1317be911e2243221919c63
Léo HUBERT <leo.hubert@bodyguard.ai>
2023-05-12 11:33:39 +0200

    V2 (#1)

--- index.js
+++ index.js
@@ -1,18 +1,62 @@
-const TEST = require("node-global-key-listener")
+const { app, BrowserWindow, globalShortcut, dialog } = require('electron')
 var player = require('play-sound')(opts = {})
+const path = require('path')
 
-const v = new TEST.GlobalKeyboardListener();
-
+const basePath = app.isPackaged ? process.resourcesPath : __dirname 
 
 const sounds = [
-  'foo.m4a',
-  'kraaa.m4a',
+  path.resolve(basePath, 'sounds/foo.m4a'),
+  path.resolve(basePath, 'sounds/kraaa.m4a'),
+  path.resolve(basePath, 'sounds/mouette.m4a'),
+  path.resolve(basePath, 'sounds/turtle.m4a')
 ]
 
-v.addListener(function (e, down) {
-  if (e.state === "DOWN") {
-    player.play(sounds[Math.floor(Math.random()*sounds.length)], function(err){
-      if (err) throw err
+console.log("sounds",  sounds)
+
+function playSound() {
+  player.play(sounds[Math.floor(Math.random()*sounds.length)], function(err){
+    if (err) {
+      app.quit()
+      dialog.showErrorBox("Error", "Error playing sound")
+    }
+  })
+}
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 0,
+    height: 0,
+    show: false,
+    webPreferences: {nodeIntegration: true}
+  })
+  
+  if (process.platform == 'darwin') {  
+    app.dock.hide()  
+  }
+  return win
+}
+
+const keys = '`1234567890-=qwertyuiop[]asdfghjkl;\'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOPASDFGHJKL:"ZXCVBNM<>?'.split('')
+
+app.whenReady().then(() => {
+  const win = createWindow()
+
+  globalShortcut.register('OPTION+Q', () => {
+    app.quit()
+  })
+
+  for (const key of keys) {
+    globalShortcut.register(key, () => {
+      try {
+        playSound()
+      } catch (err) {
+        dialog.showErrorBox('Error', err.message)
+        app.quit()
+      }
     })
   }
-});
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
